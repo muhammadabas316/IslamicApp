f4b989d9621e322e8f6fafbca22ecd3fa372f9a0
Abbas <muhammadabas316@gmail.com>
2022-11-01 02:47:29 -0700

    Version 1.0

--- app/src/main/java/com/example/islamicapp/Fragments/PrayerDetails.kt
+++ app/src/main/java/com/example/islamicapp/Fragments/PrayerDetails.kt
@@ -4,18 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.islamicapp.R
+import com.example.islamicapp.databinding.FragmentPrayerDetailsBinding
 
 class PrayerDetails : Fragment() {
-
+    private lateinit var binding: FragmentPrayerDetailsBinding
+    private lateinit var cast : ArrayList<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_prayer_details, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_prayer_details, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.auto
     }
 
 }
